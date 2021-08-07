fe5f53964d9ace33f9e76eaa75c91616726f512a
devonly <nasawake.am@gmail.com>
2021-08-08 02:10:41 +0900

    - First commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,12 +6,81 @@
 //
 
 import SwiftUI
+import Punycode
 
 struct ContentView: View {
+    @State var text: String = "えむいーさん!のコードはUNCODE!"
+    @State var encodedMode: Bool = true
+    @State var encodedText: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            TextField("Hello, World!", text: $text)
+            Toggle(isOn: $encodedMode, label: { Text("Encode") })
+            Button(action: { encodedText = encoded(encodedMode, text) }, label: { Text(encodedMode ? "Encode" : "Decode") })
+            Text(encodedText)
+                .foregroundColor(.secondary)
+        }
     }
+    
+    @discardableResult
+    private func encoded(_ encodedMode: Bool, _ text: String) -> String {
+        // 先頭及び末尾の空白文字などを削除
+        let replacedText: String = text.trimmingCharacters(in: .whitespacesAndNewlines)
+        // Punycode化
+        let punycodeText: String = ikaEncode(text: replacedText.components(separatedBy: "!").compactMap({ $0.punycodeEncoded() }).joined(separator: "!"))
+        return encodeFromJa(from: punycodeText)
+    }
+
+    // 一行しかないときはReturnを省略できる
+    // 先頭から4行を見て判断
+    private func ikaEncode(text: String) -> String {
+        text.prefix(4) == "xn--" ? String(text.prefix(4)) : "-"
+    }
+    
+    private func encodeFromJa(from text: String) -> String {
+        encodedDictionary.reduce(text) { $0.lowercased().replacingOccurrences(of: $1.key, with: $1.value) }
+    }
+    
+    // 英語から日本語への変換用の辞書
+    private var encodedDictionary: Dictionary<String, String> = [
+        "a": "あ",
+        "b": "ん",
+        "c": "ま",
+        "d": "め",
+        "e": "み",
+        "f": "うぇ",
+        "g": "む",
+        "h": "え",
+        "i": "い",
+        "j": "お",
+        "k": "う",
+        "l": "な",
+        "m": "ぬ",
+        "n": "にゅ",
+        "o": "の",
+        "p": "ぬ",
+        "q": "か",
+        "r": "け",
+        "s": "き",
+        "t": "し",
+        "u": "そ",
+        "v": "る",
+        "w": "にゃ",
+        "x": "ちぇ",
+        "y": "ひ",
+        "z": "に",
+        "0": "れぃ",
+        "1": "とぅ",
+        "2": "ら",
+        "3": "ぴ",
+        "4": "れ",
+        "5": "り",
+        "6": "わ",
+        "7": "て",
+        "8": "や",
+        "9": "ゆ",
+        "-": "っ"
+    ]
 }
 
 struct ContentView_Previews: PreviewProvider {
