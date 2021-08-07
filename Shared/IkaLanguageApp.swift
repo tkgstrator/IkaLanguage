2a17e5b80758f4d873a1d512b215b06a2f669d82
tkgstrator <tkgstrator@users.noreply.github.com>
2021-08-07 22:43:01 +0900

    Initial Commit

---
+++ Shared/IkaLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  IkaLanguageApp.swift
+//  Shared
+//
+//  Created by devonly on 2021/08/07.
+//
+
+import SwiftUI
+
+@main
+struct IkaLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
